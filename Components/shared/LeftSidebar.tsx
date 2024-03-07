d9d13af0097f5fc409b398d092ffdabc35c3aa5b
ryannnc <ry3ncao@gmail.com>
2024-03-07 18:13:21 -0500

    ignore ts errors

--- Components/shared/LeftSidebar.tsx
+++ Components/shared/LeftSidebar.tsx
@@ -4,7 +4,7 @@ import { OrganizationSwitcher, SignOutButton, SignedIn, useAuth } from "@clerk/n
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter } from 'next/navigation';
-import {sidebarLinks} from "@/Constants/"
+import {sidebarLinks} from "@/Constants/index.js"
 function LeftSidebar() {
 
     const router = useRouter();
